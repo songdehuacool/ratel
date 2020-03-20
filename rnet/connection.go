0ddfe30d20bbebb3dc14157004aec18369f7d7c7
200637086@qq.com <200637086@qq.com>
2020-03-20 21:53:38 +0800

    v0.7

--- rnet/connection.go
+++ rnet/connection.go
@@ -31,8 +31,11 @@ type Connection struct {
 	// 当前的链接状态
 	isClosed bool
 
-	// 告知当前链接已经退出/停止 channel
+	// 告知当前链接已经退出/停止 channel(由Reader告知退出)
 	ExitChan chan bool
+
+	// 无缓冲的管道，用于读、写Groutine之间读消息通信
+	msgChan chan []byte
 
 	// 消息的管理MsgID 和对应的处理业务API关系
 	MsgHandler riface.IMsgHandler
@@ -46,14 +49,39 @@ func NewConnection(conn *net.TCPConn, connID uint32, msgHandler riface.IMsgHandl
 		isClosed:   false,
 		MsgHandler: msgHandler,
 		ExitChan:   make(chan bool, 1),
+		msgChan:    make(chan []byte),
 	}
 	return c
+}
+
+/*
+	写消息Goroutine，专门发送给客户端消息模块
+*/
+func (c *Connection) StartWrite() {
+	fmt.Println("[Write Goroutine is running...]")
+	defer fmt.Println(c.RemoteAddr().String(), " [conn Writer exit!]")
+
+	// 不断的阻塞的等待channel的消息，进行写给客户端
+	for {
+		select {
+		case data := <-c.msgChan:
+			// 有数据写给客户端
+			if _, err := c.Conn.Write(data); err != nil {
+				fmt.Println("Send data error, ", err)
+				return
+			}
+		case <-c.ExitChan:
+			// 代表Reader已经退出，此时Write也要退出
+			return
+
+		}
+	}
 }
 
 // 链接的读业务方法
 func (c *Connection) StartReader() {
 	fmt.Println("Reader Goroutine is running...")
-	defer fmt.Println("connID = ", c.ConnID, " Reader is exit, remote addr is ", c.RemoteAddr().String())
+	defer fmt.Println("connID = ", c.ConnID, " [Reader is exit], remote addr is ", c.RemoteAddr().String())
 	defer c.Stop()
 
 	for {
@@ -118,10 +146,7 @@ func (c *Connection) SendMsg(msgId uint32, data []byte, int2 int) error {
 	}
 
 	// 将数据发送给客户端
-	if _, err := c.Conn.Write(binaryMsg); err != nil {
-		fmt.Println("Write msg id ", msgId, " error: ", err)
-		return errors.New("conn Write error")
-	}
+	c.msgChan <- binaryMsg
 	return nil
 }
 
@@ -131,7 +156,7 @@ func (c *Connection) Start() {
 	// 启动从当前链接的读数据的业务
 	go c.StartReader()
 	// TODO 启动从当前链接写数据的业务
-
+	go c.StartWrite()
 }
 
 // 停止链接 结束当前链接的工作
@@ -145,11 +170,14 @@ func (c *Connection) Stop() {
 	c.isClosed = true
 
 	// 关闭socket链接
-
 	c.Conn.Close()
+
+	// 告知Write关闭
+	c.ExitChan <- true
 
 	// 回收资源
 	close(c.ExitChan)
+	close(c.msgChan)
 }
 
 // 获取当前链接的绑定sockert conn
